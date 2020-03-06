c522f37edd5737e01ee9fb8618beb2dd6f31e4d0
Couapy <couapy.web@gmail.com>
2020-03-06 11:35:07 +0100

    V3

--- update.py
+++ update.py
@@ -13,31 +13,10 @@ class Domain:
         self.domain = domain
         self.auth = (user, password)
         self.device_ip = ''
-        self.server_ip = ''
 
     def update(self, device_ip):
-        """Update the dynhost of the domain"""
-        self.device_ip = device_ip
-        error = False
-        try:
-            req = resquests.get(URL_UPDATE_IP)
-            self.server_ip = req.text
-        except Exception as e:
-            error = True
-
-        if error:
-            print('[ERROR]Impossible de récupérer l\'adresse ip du serveur')
-        elif self.server_ip != self.device_ip:
-            print()
-            print('[WARN]Le domaine "{}" n\'est pas à jour'.format(self.domain))
-            self.update_dynhost(device_ip)
-        else:
-            print('[INFO]Le domaine "{}" est à jour'.format(self.domain))
-
-    def update_dynhost(self, device_ip):
         """Update online the DNS"""
-        print('[INFO]IP actuelle : {}'.format(self.server_ip))
-        print('[INFO]IP nouvelle : {}'.format(self.device_ip))
+        print('[INFO]IP : {}'.format(self.device_ip))
         print('[INFO]Mise à jour en cours...')
         req = requests.get(URL_UPDATE.format(
             self.domain, device_ip), auth=self.auth)
@@ -51,7 +30,6 @@ class Domain:
 
 # CONFIG
 URL_UPDATE = 'http://www.ovh.com/nic/update?system=dyndns&hostname={}&myip={}'
-URL_UPDATE_IP = 'https://ifconfig.me/ip'
 
 # Config parser
 config = configparser.ConfigParser()
@@ -62,9 +40,8 @@ domains = []
 device_ip = ''
 
 # Update device ip
-req = requests.get('http://ifconfig.co/json')
-response = json.loads(req.text)
-device_ip = response['ip']
+req = requests.get('http://ifconfig.co/ip')
+device_ip = req.text
 
 for domain in config.sections():
     domains.append(Domain(domain, config[domain]['user'], config[domain]['password']))
